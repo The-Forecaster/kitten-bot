31f67a82d0803835e99f8389dc402c0b77c0d87a
the-forecaster <DarkDruid11@gmail.com>
2022-08-08 22:02:58 -0600

    initial commit

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const embed = new EmbedBuilder()
     client.once('ready', async () => {
         const channel = client.channels.cache.get(webhookClient.token);
         try {
-            const webhooks = await channel.fetchWebhooks();
-            const webhook = webhooks.find(wh => wh.token);
+            const webhook = channel.fetchWebhooks().find(wh => wh.token);
     
             if (!webhook) {
                 return console.log('No webhook was found that I can use!');
