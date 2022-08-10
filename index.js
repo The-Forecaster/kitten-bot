32c917ec752a0fb5fe98d3544215f720d6fe136b
Austin the Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-08-09 20:05:26 -0600

    random stuff

--- index.js
+++ index.js
@@ -8,24 +8,16 @@ const embed = new EmbedBuilder()
 	.setTitle('Kitten Bot')
 	.setColor(0x00FFFF);
 
-
-    client.once('ready', async () => {
-        const channel = client.channels.cache.get(webhookClient.token);
-        try {
-            const webhook = channel.fetchWebhooks().find(wh => wh.token);
-    
-            if (!webhook) {
-                return console.log('No webhook was found that I can use!');
-            }
-    
-            await webhook.send({
-                content: 'Webhook test',
-                username: 'kitten-bot',
-                embeds: [embed],
-            });
-        } catch (error) {
-            console.error('Error trying to send a message: ', error);
-        }
-    });
+client.once('ready', async () => {
+    try {
+        await webhookClient.send({
+            content: 'Test',
+            username: 'kitten-bot',
+            embeds: [embed],
+        });
+    } catch (error) {
+        console.error('Error trying to send a message: ', error);
+    }
+});
 
 client.login(token);
