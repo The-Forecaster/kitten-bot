825c21d151286267d34fba557efb5b4074e6b1ef
The-Forecaster <DarkDruid11@gmail.com>
2022-12-04 00:58:39 -0700

    small updates

--- index.js
+++ index.js
@@ -4,28 +4,26 @@ const webhookClient = new WebhookClient({ url: webhook })
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const embed = new EmbedBuilder()
-	.setTitle('Kitten Bot')
-	.setColor(0x00FFFF);
+const embed = new EmbedBuilder().setTitle('Kitten Bot').setColor(0x00FFFF);
 
 
-    client.once('ready', async () => {
-        const channel = client.channels.cache.get(webhookClient.token);
-        try {
-            const webhook = channel.fetchWebhooks().find(wh => wh.token);
-    
-            if (!webhook) {
-                return console.log('No webhook was found that I can use!');
-            }
-    
-            await webhook.send({
-                content: 'Webhook test',
-                username: 'kitten-bot',
-                embeds: [embed],
-            });
-        } catch (error) {
-            console.error('Error trying to send a message: ', error);
+client.once('ready', async () => {
+    const channel = client.channels.cache.get(webhookClient.token);
+    try {
+        const webhook = channel.fetchWebhooks().find(wh => wh.token);
+
+        if (!webhook) {
+            return console.log('No webhook was found that I can use!');
         }
-    });
+
+        await webhook.send({
+            content: 'Webhook test',
+            username: 'kitten-bot',
+            embeds: [embed],
+        });
+    } catch (error) {
+        console.error('Error trying to send a message: ', error);
+    }
+});
 
 client.login(token);
